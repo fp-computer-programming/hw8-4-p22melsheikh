1a1a8b80873c49bb5b2e7c592040e702128e01d0
x <xxx@tsststs.com>
2021-12-10 00:34:29 -0500

    hw
    hw

---
+++ hw8-4-1.py
@@ -0,0 +1,21 @@
+# MEE 12/09/21
+
+
+import random
+
+number = random.randint(0, 50)
+
+while True:
+    user_number = input("Guess a number from 0-50. ")
+    if user_number == "stop":
+        print("The random number was " + str(number))
+        break
+    if int(user_number) > number:
+        print("Guess lower")
+        continue
+    elif int(user_number) < number:
+        print("Guess higher")
+        continue
+    elif int(user_number) == number:
+        print("You guessed the correct number!")
+        break
